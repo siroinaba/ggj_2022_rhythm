d6f4a7eb68c802c2aab25df79946e31da6641232
@siroinaba <siroinaba@gmail.com>
2022-01-28 23:52:42 +0900

    色々

---
+++ Assets/NotesAssembly.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotesAssembly : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NotesMove()
+    {
+
+    }
+
+    [SerializeField]
+    private GameObject _lightningNote;
+
+    [SerializeField]
+    private GameObject _darknessNote;
+
+    private List<GameObject> NotesList;
+}
