f317e163a0ebb4ddeb438797adfe997d037daf63
@siroinaba <siroinaba@gmail.com>
2022-01-29 09:11:38 +0900

    ノーツ動きテスト

---
+++ Assets/NoteEntity.cs
@@ -0,0 +1,58 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteEntity : MonoBehaviour
+{
+    public bool isActivate { get { return _isActivate; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Initialize()
+    {
+        _mesh = GetComponent<MeshRenderer>();
+        _mesh.enabled = false;
+
+        _isActivate = false;
+    }
+
+    public void Move()
+    {
+        Debug.Log(Time.deltaTime);
+
+        Vector3 vec = this.transform.position;
+        vec.z -= 1.0f;
+
+        this.transform.DOMove(vec, 0.5f);
+    }
+
+    public void SetActivate(bool isActivate, Vector3 pos)
+    {
+        _mesh.enabled = isActivate;
+        _isActivate = isActivate;
+
+        if (isActivate)
+        {
+            SetPos(pos);
+        }
+    }
+
+    private void SetPos(Vector3 pos)
+    {
+        transform.localPosition = pos;
+    }
+
+    MeshRenderer _mesh;
+    bool _isActivate = false;
+}
--- Assets/NotesAssembly.cs
+++ Assets/NotesAssembly.cs
@@ -4,21 +4,83 @@ using UnityEngine;
 
 public class NotesAssembly : MonoBehaviour
 {
+    public const int LEN_MAX = 10;
+    public const int NOTES_MAX = 100;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //NoteCreate();
+        NotesLoad();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(test());
+
+        }
+    }
+
+    private void NotesLoad()
+    {
+        for(int i = 0; i < NOTES_MAX; i++)
+        {
+            NoteCreate();
+
+        }
+    }
+
+    public void NoteCreate()
+    {
+        var note = Instantiate(_lightningNote, Vector3.zero, Quaternion.identity).GetComponent<NoteEntity>();
+        note.Initialize();
+        NotesList.Add(note);
+    }
+
+    private void ActivateNote()
+    {
+        for(int i = 0; i < NOTES_MAX; i++)
+        {
+            if (!NotesList[i].isActivate)
+            {
+                Debug.Log(i);
+                int lenIdx = Random.Range(0, LEN_MAX);
+                NotesList[i].SetActivate(true, _lenPosList[lenIdx]);
+
+                break;
+            }
+        }
     }
 
     public void NotesMove()
     {
+        for(int i = 0; i < NotesList.Count; i++)
+        {
+            if (!NotesList[i].isActivate)
+                continue;
 
+            NotesList[i].Move();
+        }
+    }
+
+    private IEnumerator test()
+    {
+        int count = 0;
+        while(count < 10)
+        {
+            ActivateNote();
+            NotesMove();
+            count++;
+            yield return new WaitForSeconds(1.0f);
+        }
+
+        yield break;
     }
 
     [SerializeField]
@@ -27,5 +89,8 @@ public class NotesAssembly : MonoBehaviour
     [SerializeField]
     private GameObject _darknessNote;
 
-    private List<GameObject> NotesList;
+    [SerializeField]
+    private List<Vector3> _lenPosList = new List<Vector3>();
+
+    private List<NoteEntity> NotesList = new List<NoteEntity>();
 }
